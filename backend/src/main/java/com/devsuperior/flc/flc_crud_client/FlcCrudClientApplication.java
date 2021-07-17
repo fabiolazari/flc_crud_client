e58ca8932e489aa229bae16db0e469a20937c00a
Fabio Lazari Costa <lazari.developer@gmail.com>
2021-07-17 12:31:00 -0300

    Backend project create

---
+++ backend/src/main/java/com/devsuperior/flc/flc_crud_client/FlcCrudClientApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.flc.flc_crud_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlcCrudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlcCrudClientApplication.class, args);
+	}
+
+}
