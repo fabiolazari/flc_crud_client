3e4aad3ce2d9aed5cc930f118f85b26a243c5055
Fabio Lazari Costa <lazari.developer@gmail.com>
2021-07-19 18:02:28 -0300

    Implementado insert cliente

--- backend/src/main/java/com/devsuperior/flc/flc_crud_client/dto/ClientDTO.java
+++ backend/src/main/java/com/devsuperior/flc/flc_crud_client/dto/ClientDTO.java
@@ -1,6 +1,7 @@
 package com.devsuperior.flc.flc_crud_client.dto;
 
 import java.io.Serializable;
+import java.time.Instant;
 
 import com.devsuperior.flc.flc_crud_client.entities.Client;
 
@@ -12,16 +13,18 @@ public class ClientDTO implements Serializable {
 	private String name;
 	private String cpf;
 	private Double income;
+	private Instant birthDate;
 	private Integer children;
 	
 	public ClientDTO() {
 	}
 
-	public ClientDTO(Long id, String name, String cpf, Double income, Integer children) {
+	public ClientDTO(Long id, String name, String cpf, Double income, Integer children, Instant birthDate) {
 		this.id = id;
 		this.name = name;
 		this.cpf = cpf;
 		this.income = income;
+		this.birthDate= birthDate;
 		this.children = children;
 	}
 
@@ -30,6 +33,7 @@ public class ClientDTO implements Serializable {
 		this.name = client.getName();
 		this.cpf = client.getCpf();
 		this.income = client.getIncome();
+		this.birthDate= client.getBirthDate();
 		this.children = client.getChildren();
 	}
 	
@@ -63,6 +67,14 @@ public class ClientDTO implements Serializable {
 
 	public void setIncome(Double income) {
 		this.income = income;
+	}
+	
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
 	}
 
 	public Integer getChildren() {
